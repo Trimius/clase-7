93cf3904f62fe06d7fb79da80392771c02ea1c6a
Jeff <jedayr9074@hotmail.com>
2019-05-30 21:36:47 -0500

    third commit

--- MateriasReprobadas.js
+++ MateriasReprobadas.js
@@ -1,43 +1,37 @@
 let alumnos = [{
     nombre: 'Carlos',
-    materias = [{
+    materias: [{
         clase: 'Matematicas',
-        calificaciones: [8, 7, 9]
+        calificaciones: 8
     },{
         clase: 'Biologia',
-        calificaciones: [5, 5, 6]
+        calificaciones: 5
     },{
         clase: 'Informatica',
-        calificaciones: [9, 8, 10]
+        calificaciones: 9
     }],
+},{
     nombre: 'Hazel',
-    materias = [{
+    materias: [{
         clase: 'Matematicas',
-        calificaciones: [8, 9, 8]
+        calificaciones: 8
     },{
         clase: 'Biologia',
-        calificaciones: [9, 7, 10]
+        calificaciones: 9
     },{
         clase: 'Informatica',
-        calificaciones: [5, 6, 5]
+        calificaciones: 5
     }]
 }]
 
 for(let index = 0; index<alumnos.length; index++){
-    let alumno = alumnos[index]
-    alumno.materias
-    
-    for(let index2 = 0; index2<materias.length; index2++){
-        let materia = materias[index2]
-        materia.calificaciones
-        let sumaCalificaciones = 0
-
-        for(let index3 = 0; index3<calificaciones.length; index3++){
-            sumaCalificaciones += materias.calificaciones[index3]
+    let nombreAlumno = ''
+    for(let index2 = 0; index2<alumnos[index].materias.length; index2++){
+        if(alumnos[index].materias[index2].calificaciones<6){
+            nombreAlumno =alumnos[index].nombre
+            console.log(nombreAlumno);
+            console.log(alumnos[index].materias[index2]);
+            
         }
-        const promedio = sumaCalificaciones / materias.calificaciones.length
-        if(promedio<6){
-            console.log(alumno.nombre + ' reprobo ' + materia.clase + ' con un promedio de ' + promedio);
-        }
-       }
+    }
 }
